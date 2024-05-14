0a8c9c8067b66b70d417e34b5b99d4e1c77e65c3
chanho <uni070@naver.com>
2024-05-15 00:35:29 +0900

    [Build] Retrofit, mosi Dependency Add

---
+++ DaOnGil/app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-parcelize")
+    id ("kotlin-kapt")
+}
+
+android {
+    namespace = "kr.tekit.lion.daongil"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "kr.tekit.lion.daongil"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.11.0")
+
+    implementation ("com.squareup.moshi:moshi:1.15.1")
+    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+}

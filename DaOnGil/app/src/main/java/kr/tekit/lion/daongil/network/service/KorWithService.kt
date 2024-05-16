411b218061dc3384df9eb34feafaf8157382dc2e
chanho <uni070@naver.com>
2024-05-17 06:51:19 +0900

    [#3] Retrofit Service Interface API BASE URL 변경 + Service 싱글톤으로 변경

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/network/service/KorWithService.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/network/service/KorWithService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
 interface KorWithService {
-    @GET("B551011/KorWithService1/areaCode1")
+    @GET("areaCode1")
     suspend fun getAreaCode(
         @QueryMap params: Map<String, String>
     ): AreaCodeResponse

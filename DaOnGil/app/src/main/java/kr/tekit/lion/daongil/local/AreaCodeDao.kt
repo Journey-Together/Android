2f166d15c9e98ee50733bc25cdc237cb1df48407
chanho <uni070@naver.com>
2024-05-17 06:48:31 +0900

    [#3] AreaCodeDataSource Interface 메소드명 변경 + 좀 더 구분하기 쉽게 변경했슴니다

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/local/AreaCodeDao.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/local/AreaCodeDao.kt
@@ -14,13 +14,7 @@ interface AreaCodeDao {
     @Query("SELECT * FROM area_code_table WHERE code = :areaCode")
     suspend fun getAreaCode(areaCode: String): AreaCodeEntity
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAreaCode(areaCode: List<AreaCodeEntity>)
-
-    @Update
-    suspend fun updateMyPet(areaCode: AreaCodeEntity)
-
-    @Query("DELETE FROM area_code_table WHERE code = :areaCode")
-    suspend fun deleteMyPet(areaCode: String)
 }
 

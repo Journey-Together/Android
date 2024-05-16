046b7c75423e5becc292b8d0248fd7bd1491212a
chanho <uni070@naver.com>
2024-05-17 06:52:49 +0900

    [#3] API Test code

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModel.kt
@@ -5,26 +5,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import kr.tekit.lion.daongil.usecase.areacode.GetAllAreaCodeUseCase
-import kr.tekit.lion.daongil.usecase.areacode.GetAreaCodeUseCase
-import kr.tekit.lion.daongil.usecase.areacode.InitAreaCodeUseCase
+import kr.tekit.lion.daongil.usecase.areacode.GetAreaCodeInfoUseCase
+import kr.tekit.lion.daongil.usecase.areacode.InitAreaCodeInfoUseCase
 import kr.tekit.lion.daongil.usecase.base.onError
 import kr.tekit.lion.daongil.usecase.base.onSuccess
 
 class MainViewModel(
-    private val initAreaCodeUseCase: InitAreaCodeUseCase,
     private val getAllAreaCodeUseCase: GetAllAreaCodeUseCase,
-    private val getAreaCodeUseCase: GetAreaCodeUseCase
+    private val getAreaCodeInfoUseCase: GetAreaCodeInfoUseCase,
+    private val initAreaCodeInfoUseCase: InitAreaCodeInfoUseCase
 ): ViewModel() {
     init {
         viewModelScope.launch {
-            initAreaCodeUseCase(
-                "t2ivQakqcZ/cvxzekT7Ra9Ja8J1N1lBKu6LqVkijMliEeoD1lLXU0Qei+V9AC8aMbNG+TjVkca70NqFB9akmSg==",
-            ).onSuccess {
-                Log.d("ApiTestResult", it.toString() )
-            }.onError {
-                Log.d("ApiTestResult", it.toString() )
-            }
-            Log.d("ApiTestResult", getAreaCodeUseCase("1").toString() )
+            initAreaCodeInfoUseCase()
+            Log.d("ApiTestResult", getAreaCodeInfoUseCase("1").toString() )
         }
     }
 }
--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModelFactory.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModelFactory.kt
@@ -5,20 +5,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kr.tekit.lion.daongil.repository.areacode.AreaCodeRepository
 import kr.tekit.lion.daongil.usecase.areacode.GetAllAreaCodeUseCase
-import kr.tekit.lion.daongil.usecase.areacode.GetAreaCodeUseCase
-import kr.tekit.lion.daongil.usecase.areacode.InitAreaCodeUseCase
-import kr.tekit.lion.daongil.usecase.areacode.SetAreaCodeUseCase
+import kr.tekit.lion.daongil.usecase.areacode.GetAreaCodeInfoUseCase
+import kr.tekit.lion.daongil.usecase.areacode.InitAreaCodeInfoUseCase
 import java.lang.IllegalArgumentException
 
-class MainViewModelFactory(context: Context): ViewModelProvider.Factory {
+class MainViewModelFactory(context: Context) : ViewModelProvider.Factory {
     private val areaCodeRepository = AreaCodeRepository.create(context)
-    private val initAreaCodeUseCase = InitAreaCodeUseCase(areaCodeRepository, SetAreaCodeUseCase(areaCodeRepository))
     private val getAllAreaCodeUseCase = GetAllAreaCodeUseCase(areaCodeRepository)
-    private val getAreaCodeUseCase = GetAreaCodeUseCase(areaCodeRepository)
+    private val getAreaCodeInfoUseCase = GetAreaCodeInfoUseCase(areaCodeRepository)
+    private val initAreaCodeInfoUseCase = InitAreaCodeInfoUseCase(areaCodeRepository)
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)){
-            return MainViewModel(initAreaCodeUseCase, getAllAreaCodeUseCase, getAreaCodeUseCase) as T
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(
+                getAllAreaCodeUseCase,
+                getAreaCodeInfoUseCase,
+                initAreaCodeInfoUseCase
+            ) as T
         }
         throw IllegalArgumentException("unknown ViewModel class")
     }

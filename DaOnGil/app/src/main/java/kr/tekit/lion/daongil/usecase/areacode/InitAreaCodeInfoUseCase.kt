4eb4a1c11acac8aa1c08f4786b6679d7eded148d
chanho <uni070@naver.com>
2024-05-19 03:07:03 +0900

    [#7] 기타 수정 패키지 수정, 잘못된 메소드명 변경, 불필요한 코드 제거

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/usecase/areacode/InitAreaCodeInfoUseCase.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/usecase/areacode/InitAreaCodeInfoUseCase.kt
@@ -8,7 +8,7 @@ class InitAreaCodeInfoUseCase(
     private val areaCodeRepository: AreaCodeRepository,
 ): BaseUseCase() {
     suspend operator fun invoke(): Result<Unit> = execute {
-        areaCodeRepository.fetchAreaCodeInfo()
-        areaCodeRepository.fetchVillageCodeInfo()
+        areaCodeRepository.addAreaCodeInfo()
+        areaCodeRepository.addVillageCodeInfo()
     }
 }

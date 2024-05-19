40029fa80bb710e29428a3096d18891f024569bd
chanho <uni070@naver.com>
2024-05-20 03:50:15 +0900

    [#11] 지역 기반 조회 DTO

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/dto/request/AreaCodeRequest.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/dto/request/AreaCodeRequest.kt
@@ -1,29 +1,22 @@
 package kr.tekit.lion.daongil.dto.request
 
-data class AreaCodeRequest(
-    val numOfRows: String = API_CODES_MAX_NUM_OF_ROWS,
-    val mobileOs: String = API_MOBILE_OS,
-    val mobileApp: String = APP_NAME,
-    val type: String = API_TYPE,
-    val areaCode: String,
-    val serviceKey: String = SERVICE_KEY,
-){
-    fun toRequestModel(): Map<String, String> {
-        return mapOf(
-            "numOfRows" to numOfRows,
-            "MobileOS" to mobileOs,
-            "MobileApp" to mobileApp,
-            "areaCode" to areaCode,
-            "_type" to type,
-            "serviceKey" to serviceKey,
-        )
-    }
+import kr.tekit.lion.daongil.dto.base.ApiConstants.Companion.API_CODES_MAX_NUM_OF_ROWS
+import kr.tekit.lion.daongil.dto.base.ApiConstants.Companion.API_MOBILE_OS
+import kr.tekit.lion.daongil.dto.base.ApiConstants.Companion.API_TYPE
+import kr.tekit.lion.daongil.dto.base.ApiConstants.Companion.APP_NAME
+import kr.tekit.lion.daongil.dto.base.ApiConstants.Companion.SERVICE_KEY
 
-    companion object{
-        const val API_CODES_MAX_NUM_OF_ROWS = "31"
-        const val API_MOBILE_OS = "AND"
-        const val API_TYPE = "json"
-        const val APP_NAME = "DaOnGil"
-        const val SERVICE_KEY = "t2ivQakqcZ/cvxzekT7Ra9Ja8J1N1lBKu6LqVkijMliEeoD1lLXU0Qei+V9AC8aMbNG+TjVkca70NqFB9akmSg=="
+data class AreaCodeRequest(
+    val areaCode: String,
+){
+    fun toRequestModel(): Map<String, String>{
+        return mapOf(
+            "numOfRows" to API_CODES_MAX_NUM_OF_ROWS,
+            "MobileOS" to API_MOBILE_OS,
+            "MobileApp" to APP_NAME,
+            "_type" to API_TYPE,
+            "serviceKey" to SERVICE_KEY,
+            "areaCode" to areaCode
+        )
     }
 }

5e53ef5ab9ed4bf28c88a8285d48e2a3625b1b1b
chanho <uni070@naver.com>
2024-05-22 11:15:31 +0900

    [Chore] 일정 프래그먼트명 변경

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/fragment/ScheduleMainFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/fragment/ScheduleMainFragment.kt
@@ -2,17 +2,15 @@ package kr.tekit.lion.daongil.presentation.main.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import kr.tekit.lion.daongil.R
-import kr.tekit.lion.daongil.databinding.FragmentCalenderMainBinding
+import kr.tekit.lion.daongil.databinding.FragmentScheduleMainBinding
 
-class CalenderMainFragment : Fragment(R.layout.fragment_calender_main) {
+class ScheduleMainFragment : Fragment(R.layout.fragment_schedule_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding = FragmentCalenderMainBinding.bind(view)
+        val binding = FragmentScheduleMainBinding.bind(view)
     }
 }

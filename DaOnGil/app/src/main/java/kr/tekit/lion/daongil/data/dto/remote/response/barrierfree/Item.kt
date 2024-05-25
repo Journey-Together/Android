6e8f78ac8bf0748382b07af39c73f02c06bc252d
chanho <uni070@naver.com>
2024-05-26 02:39:39 +0900

    [#19] 무장애 정보 응답 모델 수정

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/barrierfree/Item.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/barrierfree/Item.kt
@@ -6,55 +6,52 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Item(
-    @Json(name = "audioguide")
-    val audioGuide: String,
-    @Json(name = "auditorium")
-    val auditorium: String,
-    @Json(name = "babysparechair")
-    val babySpareChair: String,
-    @Json(name = "bigprint")
-    val bigPrint: String,
-    @Json(name = "blindhandicapetc")
-    val blindHandicapEtc: String,
-    @Json(name = "braileblock")
-    val braileBlock: String,
-    @Json(name = "brailepromotion")
-    val brailePromotion: String,
     @Json(name = "contentid")
     val contentId: String,
-    @Json(name = "elevator")
-    val elevator: String,
-    @Json(name = "exit")
-    val exit: String,
-    @Json(name = "guidehuman")
-    val guideHuman: String,
-    @Json(name = "guidesystem")
-    val guideSystem: String,
-    @Json(name = "handicapetc")
-    val handicapEtc: String,
-    @Json(name = "hearinghandicapetc")
-    val hearingHandicapEtc: String,
-    @Json(name = "hearingroom")
-    val hearingRoom: String,
-    @Json(name = "helpdog")
-    val helpDog: String,
-    @Json(name = "infantsfamilyetc")
-    val infantsFamilyEtc: String,
-    @Json(name = "lactationroom")
-    val lactationRoom: String,
     val parking: String,
-    val promotion: String,
+    val route: String,
     @Json(name = "publictransport")
     val publicTransport: String,
-    val restroom: String,
-    val room: String,
-    val route: String,
-    @Json(name = "signguide")
-    val signGuide: String,
-    val stroller: String,
     @Json(name = "ticketoffice")
     val ticketOffice: String,
+    val promotion: String,
+    val wheelchair: String,
+    val exit: String,
+    val elevator: String,
+    val restroom: String,
+    val auditorium: String,
+    val room: String,
+    @Json(name = "handicapetc")
+    val handicapEtc: String,
+    @Json(name = "braileblock")
+    val braileBlock: String,
+    @Json(name = "helpdog")
+    val helpDog: String,
+    @Json(name = "guidehuman")
+    val guideHuman: String,
+    @Json(name = "audioguide")
+    val audioGuide: String,
+    @Json(name = "bigprint")
+    val bigPrint: String,
+    @Json(name = "brailepromotion")
+    val brailePromotion: String,
+    @Json(name = "guidesystem")
+    val guideSystem: String,
+    @Json(name = "blindhandicapetc")
+    val blindHandicapEtc: String,
+    @Json(name = "signguide")
+    val signGuide: String,
     @Json(name = "videoguide")
     val videoGuide: String,
-    val wheelchair: String
+    @Json(name = "hearingroom")
+    val hearingRoom: String,
+    @Json(name = "hearinghandicapetc")
+    val hearingHandicapEtc: String,
+    val stroller: String,
+    @Json(name = "lactationroom")
+    val lactationRoom: String,
+    @Json(name = "babysparechair")
+    val babySpareChair: String,
+    @Json(name = "infantsfamilyetc")
+    val infantsFamilyEtc: String,
 )

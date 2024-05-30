8595ff1654692eec3004944c3c87095e9d5515f4
rmadmsjm <dmswjdals12@naver.com>
2024-05-30 13:12:10 +0900

    [Feat] #34 닉네임 유효성 검사

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myinfo/fragment/PersonalInfoModifyFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myinfo/fragment/PersonalInfoModifyFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.FragmentPersonalInfoModifyBinding
+import kr.tekit.lion.daongil.presentation.ext.showSoftInput
 
 class PersonalInfoModifyFragment : Fragment(R.layout.fragment_personal_info_modify) {
 
@@ -18,6 +20,7 @@ class PersonalInfoModifyFragment : Fragment(R.layout.fragment_personal_info_modi
 
         initView(binding)
         PersonalInfoModify(binding)
+        setupErrorHandling(binding)
     }
 
     private fun initView(binding: FragmentPersonalInfoModifyBinding) {
@@ -34,8 +37,10 @@ class PersonalInfoModifyFragment : Fragment(R.layout.fragment_personal_info_modi
     private fun PersonalInfoModify(binding: FragmentPersonalInfoModifyBinding) {
         with(binding) {
             buttonPersonalInfoSubmit.setOnClickListener {
-                showSnackbar(binding, "개인 정보가 수정되었습니다.")
-                findNavController().navigate(R.id.action_personalInfoModifyFragment_to_myInfoFragment, null)
+                if (isFormValid(binding)) {
+                    showSnackbar(binding, "개인 정보가 수정되었습니다.")
+                    findNavController().navigate(R.id.action_personalInfoModifyFragment_to_myInfoFragment, null)
+                }
             }
         }
     }
@@ -44,5 +49,26 @@ class PersonalInfoModifyFragment : Fragment(R.layout.fragment_personal_info_modi
         Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
             .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.text_secondary))
             .show()
+    }
+
+    private fun isFormValid(binding: FragmentPersonalInfoModifyBinding): Boolean {
+        var isValid = true
+
+        if (binding.textFieldUserNickname.text.isNullOrBlank()) {
+            view?.requestFocus()
+            context?.showSoftInput(binding.textFieldUserNickname)
+            binding.textInputLayoutUserNickname.error = "닉네임을 입력해주세요."
+            isValid = false
+        }
+
+        return isValid
+    }
+
+    private fun setupErrorHandling(binding: FragmentPersonalInfoModifyBinding) {
+        binding.apply {
+            textFieldUserNickname.doOnTextChanged { text, start, before, count ->
+                textInputLayoutUserNickname.isErrorEnabled = false
+            }
+        }
     }
 }

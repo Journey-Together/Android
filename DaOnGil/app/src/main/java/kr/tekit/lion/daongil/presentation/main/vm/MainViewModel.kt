b78b8b58ab3dabbea259631d89cab03c8bb10bbc
chanho <uni070@naver.com>
2024-05-30 14:34:52 +0900

    [Add] 테스트 액티비티 생성, 기존 API 테스트용 코드 제거

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModel.kt
@@ -6,11 +6,6 @@ import kotlinx.coroutines.launch
 import kr.tekit.lion.daongil.domain.usecase.areacode.InitAreaCodeInfoUseCase
 
 class MainViewModel(
-    private val initAreaCodeInfoUseCase: InitAreaCodeInfoUseCase
 ) : ViewModel() {
-    init {
-        viewModelScope.launch {
-            initAreaCodeInfoUseCase()
-        }
-    }
+
 }

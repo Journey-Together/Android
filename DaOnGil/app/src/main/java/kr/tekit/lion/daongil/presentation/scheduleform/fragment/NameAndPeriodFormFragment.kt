f2e409df8e34b1d79cd8e798655efe12af4802cf
Boram <dev.lboram@gmail.com>
2024-05-30 14:54:57 +0900

    [Test] #38 다음 Fragment로 넘어가는 기능 테스트

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/NameAndPeriodFormFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/NameAndPeriodFormFragment.kt
@@ -3,6 +3,7 @@ package kr.tekit.lion.daongil.presentation.scheduleform.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.FragmentNameAndPeriodFormBinding
 
@@ -12,5 +13,10 @@ class NameAndPeriodFormFragment : Fragment(R.layout.fragment_name_and_period_for
 
         val binding = FragmentNameAndPeriodFormBinding.bind(view)
 
+        binding.temButton.setOnClickListener {
+            val navController = findNavController()
+            // 전환할 화면의 id (graph에 선언된 id)를 넣어준다.
+            navController.navigate(R.id.scheduleDetailsFormFragment)
+        }
     }
 }

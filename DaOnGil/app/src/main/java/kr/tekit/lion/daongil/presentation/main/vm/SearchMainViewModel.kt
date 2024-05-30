2c1e0a2081851d85a16d54728f30dc507648ea29
chanho <uni070@naver.com>
2024-05-30 14:39:55 +0900

    [Feat] 네이버 지도 연동, 장애 유형 카테고리 바텀시트

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/SearchMainViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/SearchMainViewModel.kt
@@ -1,4 +1,18 @@
 package kr.tekit.lion.daongil.presentation.main.vm
 
-class SearchMainViewMdoel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class SearchMainViewModel: ViewModel() {
+    private val _physicalDisability = MutableStateFlow<List<String>>(emptyList())
+    val physicalDisability: StateFlow<List<String>> get() = _physicalDisability
+
+    fun onSelectPhysicalDisabilityOption(options: List<String>){
+        _physicalDisability.value = options
+    }
+
+    fun onClickResetIcon(){
+        _physicalDisability.value = emptyList()
+    }
 }

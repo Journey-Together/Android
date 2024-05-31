88a1950be70b11d107ede70b4b82623a0be983af
rmadmsjm <dmswjdals12@naver.com>
2024-05-31 17:54:29 +0900

    [Feat] #34 드롭다운 및 텍스트 필드 설정

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myinfo/fragment/IceModifyFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myinfo/fragment/IceModifyFragment.kt
@@ -1,9 +1,13 @@
 package kr.tekit.lion.daongil.presentation.myinfo.fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
@@ -22,20 +26,51 @@ class IceModifyFragment : Fragment(R.layout.fragment_ice_modify) {
     }
 
     private fun initView(binding: FragmentIceModifyBinding) {
-        with(binding) {
-            toolbarIceModify.apply {
-                setNavigationIcon(R.drawable.back_icon)
-                setNavigationOnClickListener {
-                    findNavController().navigate(R.id.action_iceModifyFragment_to_myInfoFragment, null)
-                }
+        binding.toolbarIceModify.apply {
+            setNavigationIcon(R.drawable.back_icon)
+            setNavigationOnClickListener {
+                findNavController().navigate(R.id.action_iceModifyFragment_to_myInfoFragment, null)
             }
         }
 
+        settingTextField(binding)
         handleTextFieldEditorActions(binding)
+    }
+
+    private fun settingTextField(binding: FragmentIceModifyBinding) {
+        val bloodType = resources.getStringArray(R.array.blood_type)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item_blood_type, bloodType)
+
+        with(binding.textFieldIceBloodType) {
+            setDropDownBackgroundResource(R.color.background_color)
+            setAdapter(arrayAdapter)
+
+            setOnClickListener {
+                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(it.windowToken, 0)
+
+                showDropDown()
+            }
+        }
     }
 
     private fun handleTextFieldEditorActions(binding: FragmentIceModifyBinding) {
         with(binding) {
+            textFieldIceBirthday.setOnEditorActionListener { v, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_DONE ||
+                    event != null && event.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+
+                    val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+
+                    textFieldIceBirthday.clearFocus()
+
+                    true
+                } else {
+                    false
+                }
+            }
+
             textFieldIceRelation1.apply {
                 setOnEditorActionListener { v, actionId, event ->
                     textFieldIceContact1.requestFocus()
@@ -61,12 +96,11 @@ class IceModifyFragment : Fragment(R.layout.fragment_ice_modify) {
     }
 
     private fun IceInfoModify(binding: FragmentIceModifyBinding) {
-        with(binding) {
-            buttonIceSubmit.setOnClickListener {
-                showSnackbar(binding, "나의 응급 정보가 수정되었습니다.")
-                findNavController().navigate(R.id.action_iceModifyFragment_to_myInfoFragment, null)
-            }
+        binding.buttonIceSubmit.setOnClickListener {
+            showSnackbar(binding, "나의 응급 정보가 수정되었습니다.")
+            findNavController().navigate(R.id.action_iceModifyFragment_to_myInfoFragment, null)
         }
+
     }
 
     private fun showSnackbar(binding: FragmentIceModifyBinding, message: String) {

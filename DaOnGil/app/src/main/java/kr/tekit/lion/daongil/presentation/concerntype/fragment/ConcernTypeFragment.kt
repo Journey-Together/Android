30d0c9a443cfe65eca9678f74c7c4780bd98e517
rmadmsjm <dmswjdals12@naver.com>
2024-06-01 19:52:22 +0900

    [Feat] #52 사용자의 관심 유형에 따라 이미지 초기화

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/concerntype/fragment/ConcernTypeFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/concerntype/fragment/ConcernTypeFragment.kt
@@ -3,9 +3,11 @@ package kr.tekit.lion.daongil.presentation.concerntype.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.FragmentConcernTypeBinding
+import kr.tekit.lion.daongil.databinding.FragmentConcernTypeModifyBinding
 
 class ConcernTypeFragment : Fragment(R.layout.fragment_concern_type) {
 
@@ -15,6 +17,7 @@ class ConcernTypeFragment : Fragment(R.layout.fragment_concern_type) {
         val binding = FragmentConcernTypeBinding.bind(view)
 
         initView(binding)
+        initSelection(binding)
         moveConcernTypeModify(binding)
     }
 
@@ -27,6 +30,24 @@ class ConcernTypeFragment : Fragment(R.layout.fragment_concern_type) {
 
             textViewConcernTypeUseNickname.text = "다온길"
         }
+    }
+
+    private fun initSelection(binding: FragmentConcernTypeBinding) {
+        val userConcernType = listOf("고령자")
+
+        userConcernType.forEach { concernType ->
+            when (concernType) {
+                "지체장애" -> settingSelected(binding.imageViewConcernTypePhysical, R.drawable.sv_selected_physical_disability_icon)
+                "시각장애" -> settingSelected(binding.imageViewConcernTypeVisual, R.drawable.sv_selected_visual_impairment_icon)
+                "청각장애" -> settingSelected(binding.imageViewConcernTypeHearing, R.drawable.sv_selected_hearing_impairment_icon)
+                "영유아 가족" -> settingSelected(binding.imageViewConcernTypeInfant, R.drawable.sv_selected_infant_family_icon)
+                "고령자" -> settingSelected(binding.imageViewConcernTypeElderly, R.drawable.sv_selected_elderly_people_icon)
+            }
+        }
+    }
+
+    private fun settingSelected(imageView: ImageView, selectedDrawable: Int) {
+        imageView.setImageResource(selectedDrawable)
     }
 
     private fun moveConcernTypeModify(binding: FragmentConcernTypeBinding) {

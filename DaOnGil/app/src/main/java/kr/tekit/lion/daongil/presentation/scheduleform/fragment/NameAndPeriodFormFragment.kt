2a07b92c843261436496fc64c50601556a459424
Boram <dev.lboram@gmail.com>
2024-06-03 00:48:12 +0900

    [Design] #45 여행일정-제목,기간 설정 화면 UI 구현

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/NameAndPeriodFormFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/NameAndPeriodFormFragment.kt
@@ -1,11 +1,15 @@
 package kr.tekit.lion.daongil.presentation.scheduleform.fragment
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.FragmentNameAndPeriodFormBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NameAndPeriodFormFragment : Fragment(R.layout.fragment_name_and_period_form) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -13,10 +17,40 @@ class NameAndPeriodFormFragment : Fragment(R.layout.fragment_name_and_period_for
 
         val binding = FragmentNameAndPeriodFormBinding.bind(view)
 
-        binding.temButton.setOnClickListener {
+        binding.buttonNPFNextStep.setOnClickListener {
             val navController = findNavController()
             // 전환할 화면의 id (graph에 선언된 id)를 넣어준다.
             navController.navigate(R.id.scheduleDetailsFormFragment)
         }
+
+        setPeriod(binding)
+    }
+
+    private fun setPeriod(binding: FragmentNameAndPeriodFormBinding) {
+        binding.buttonNPFSetPeriod.setOnClickListener {
+            val dateRangePicker = MaterialDatePicker.Builder.dateRangePicker()
+                .setTheme(R.style.DateRangePickerTheme)
+                .setTitleText("조회기간을 설정해주세요")
+                .build()
+
+            dateRangePicker.show(requireActivity().supportFragmentManager, "ScheduleFormSetPeriod")
+            dateRangePicker.addOnPositiveButtonClickListener {
+                showPickedDates(binding, Date(it.first), Date(it.second))
+            }
+        }
+    }
+
+    private fun formatDateValue(date: Date): String {
+        val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
+        val formattedDate = dateFormat.format(date)
+
+        return formattedDate
+    }
+
+    private fun showPickedDates(binding: FragmentNameAndPeriodFormBinding, startDate: Date, endDate: Date) {
+        val startDateFormatted = formatDateValue(startDate)
+        val endDateFormatted = formatDateValue(endDate)
+        binding.buttonNPFSetPeriod.text =
+            getString(R.string.picked_dates, startDateFormatted, endDateFormatted)
     }
 }

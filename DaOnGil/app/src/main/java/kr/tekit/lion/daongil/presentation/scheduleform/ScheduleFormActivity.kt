619a2bba47ffc7f20a7e5cbcf7a9cd2ff4f64183
Boram <dev.lboram@gmail.com>
2024-06-03 15:50:03 +0900

    [Feat] #45 툴바 - 뒤로 가는 기능 추가

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/ScheduleFormActivity.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/ScheduleFormActivity.kt
@@ -2,12 +2,38 @@ package kr.tekit.lion.daongil.presentation.scheduleform
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import kr.tekit.lion.daongil.R
+import kr.tekit.lion.daongil.databinding.ActivityScheduleFormBinding
 
 class ScheduleFormActivity : AppCompatActivity() {
+    private val binding: ActivityScheduleFormBinding by lazy {
+        ActivityScheduleFormBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_schedule_form)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentScheduleForm) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val toolbar = binding.toolbarAddSchedule
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        toolbar.setNavigationIcon(R.drawable.back_icon)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+    }
+
+    // 뒤로가기 버튼
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentScheduleForm) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }

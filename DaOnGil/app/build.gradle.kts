fe2472586111d101339066ee3a04efc3b4be4fff
Jieun <lje2000lje@naver.com>
2024-06-04 22:34:52 +0900

    [Add] #59 ratingbar 라이브러리 추가

--- DaOnGil/app/build.gradle.kts
+++ DaOnGil/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -85,4 +86,6 @@ dependencies {
     implementation ("me.relex:circleindicator:2.1.6")
 
     implementation("com.google.android.gms:play-services-location:21.2.0")
+
+    implementation ("com.github.ome450901:SimpleRatingBar:1.5.1")
 }

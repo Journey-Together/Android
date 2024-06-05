b76211dedb08a9b3f430d6b07e2ca09dfdc2c1c9
Boram <dev.lboram@gmail.com>
2024-06-05 10:32:20 +0900

    [Feat] #69 일정 화면 추가, UI 구현

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedule/ScheduleActivity.kt
@@ -0,0 +1,14 @@
+package kr.tekit.lion.daongil.presentation.schedule
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.tekit.lion.daongil.R
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_schedule)
+
+    }
+}

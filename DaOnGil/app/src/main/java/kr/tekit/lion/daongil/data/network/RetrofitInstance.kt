0de6b7474e8ab74ba0ea09baba0b58951f74ebcb
Jejun Min <alswpwns11@naver.com>
2024-06-05 14:11:43 +0900

    [Feat] #43 카카오 로그인을 위한 Retrofit LoginService & headerClient 추가

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/network/RetrofitInstance.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/network/RetrofitInstance.kt
@@ -2,6 +2,7 @@ package kr.tekit.lion.daongil.data.network
 
 import android.util.Log
 import kr.tekit.lion.daongil.data.network.service.KorWithService
+import kr.tekit.lion.daongil.data.network.service.LoginService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -26,4 +27,29 @@ object RetrofitInstance {
             .build()
             .create()
     }
+
+    val loginService: LoginService by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://13.124.100.238/") // API URL
+            .addConverterFactory(MoshiConverterFactory.create().asLenient())
+            .client(headerClient)
+            .build()
+            .create()
+    }
+
+    private val headerClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+
+                chain.proceed(request)
+            }
+            .addInterceptor(HttpLoggingInterceptor{
+                Log.d("MyOkHttpLog", it)
+            }.setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
 }

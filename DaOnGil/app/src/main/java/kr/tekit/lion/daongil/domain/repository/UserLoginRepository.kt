670869baeb302df0a7921d2625694727ed0fd37b
chanho <uni070@naver.com>
2024-06-06 08:42:19 +0900

    [Remove] #73 불필요한 파일 삭제

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/repository/UserLoginRepository.kt
+++
@@ -1,8 +0,0 @@
-package kr.tekit.lion.daongil.domain.repository
-
-import kr.tekit.lion.daongil.domain.model.UserInfo
-import kr.tekit.lion.daongil.domain.usecase.base.Result
-
-interface UserLoginRepository {
-    suspend fun loginWithKakao(LoginType: String): UserInfo
-}

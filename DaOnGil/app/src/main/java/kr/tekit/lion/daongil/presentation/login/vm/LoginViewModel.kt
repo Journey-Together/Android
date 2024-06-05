7045ddcba93f2ef0b79f5eff58832279d01c6b6d
Jejun Min <alswpwns11@naver.com>
2024-06-05 14:28:34 +0900

    [Feat] #43 LoginViewModel, DataSource, RepositoryImpl 구현 부족

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/login/vm/LoginViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/login/vm/LoginViewModel.kt
@@ -1,4 +1,16 @@
 package kr.tekit.lion.daongil.presentation.login.vm
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kr.tekit.lion.daongil.domain.model.UserInfo
+import kr.tekit.lion.daongil.domain.usecase.UserLoginUseCase
+
+class LoginViewModel(
+    private val userLoginUseCase: UserLoginUseCase
+) : ViewModel() {
+
+
 }

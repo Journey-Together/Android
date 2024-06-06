232c089bfb620ca0c80c6cf18af8f82e29c46f72
Boram <dev.lboram@gmail.com>
2024-06-07 04:00:54 +0900

    [Feat] #69 내 일정에서 일정보는 화면으로 이동하는 기능

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/adapter/ScheduleMyAdapter.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/adapter/ScheduleMyAdapter.kt
@@ -6,23 +6,40 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kr.tekit.lion.daongil.databinding.RowScheduleMyBinding
 
-class ScheduleMyAdapter:RecyclerView.Adapter<ScheduleMyAdapter.ScheduleMyViewHolder>(){
-    class ScheduleMyViewHolder(private val binding: RowScheduleMyBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(){
+class ScheduleMyAdapter(private val scheduleMainItemListener: OnScheduleMainItemClickListener) :
+    RecyclerView.Adapter<ScheduleMyAdapter.ScheduleMyViewHolder>() {
+    interface OnScheduleMainItemClickListener {
+        fun onScheduleMainItemClick(scheduleIdx: Int)
+    }
+
+    class ScheduleMyViewHolder(
+        private val binding: RowScheduleMyBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(scheduleMainItemListener: OnScheduleMainItemClickListener) {
             val tempDDay = 1 // 임시 변수
-            // 다가오는 일정인 경우
-            if(tempDDay > 0){
-                binding.viewRowScheduleRemainingDeco.visibility = View.VISIBLE
-                binding.viewRowScheduleElapsedDeco.visibility = View.INVISIBLE
-                binding.textViewRowScheduleDDay.isEnabled = true // 일정 상태에 따라 TextColor 변경
-                binding.buttonRowScheduleReview.visibility = View.GONE // 후기작성 버튼 숨김처리
-            }
-            // 다녀온 일정인 경우
-            else{
-                binding.viewRowScheduleRemainingDeco.visibility = View.INVISIBLE
-                binding.viewRowScheduleElapsedDeco.visibility = View.VISIBLE
-                binding.textViewRowScheduleDDay.isEnabled = false
-                binding.buttonRowScheduleReview.visibility = View.VISIBLE
+
+            with(binding) {
+                textViewRowScheduleName.text = "제주 둘레길 여행"
+                textViewRowSchedulePeriod.text = "2024.06.01 - 2024.06.05"
+
+                // 다가오는 일정인 경우
+                if (tempDDay > 0) {
+                    viewRowScheduleRemainingDeco.visibility = View.VISIBLE
+                    viewRowScheduleElapsedDeco.visibility = View.INVISIBLE
+                    textViewRowScheduleDDay.isEnabled = true // 일정 상태에 따라 TextColor 변경
+                    textViewRowScheduleDDay.text = "D-1"
+                    buttonRowScheduleReview.visibility = View.GONE // 후기작성 버튼 숨김처리
+                }
+                // 다녀온 일정인 경우
+                else {
+                    viewRowScheduleRemainingDeco.visibility = View.INVISIBLE
+                    viewRowScheduleElapsedDeco.visibility = View.VISIBLE
+                    textViewRowScheduleDDay.isEnabled = false
+                    buttonRowScheduleReview.visibility = View.VISIBLE
+                }
+                root.setOnClickListener {
+                    scheduleMainItemListener.onScheduleMainItemClick(1)
+                }
             }
         }
     }
@@ -33,10 +50,10 @@ class ScheduleMyAdapter:RecyclerView.Adapter<ScheduleMyAdapter.ScheduleMyViewHol
     }
 
     override fun onBindViewHolder(holder: ScheduleMyViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(scheduleMainItemListener)
     }
 
     override fun getItemCount(): Int {
-        return 3
+        return 4
     }
 }

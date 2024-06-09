491c1b7b1d631202b3e8e79c53fac2d1d8611390
yein <inwon9892@naver.com>
2024-06-09 16:45:02 +0900

    [Feat] #106 AED 상세보기 화면 구현

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/EmergencyBottom.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/EmergencyBottom.kt
@@ -1,9 +1,11 @@
 package kr.tekit.lion.daongil.domain.model
 
+import java.io.Serializable
+
+
 data class EmergencyBottom(
     val emergencyList: EmergencyMapInfo?,
     val emergencyType: String,
     val emergencyId: String?,
     val aedList: AedMapInfo?
-    // 약국이랑 aed list도 추가
-)
+): Serializable
--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/emergency/fragment/AedInfoFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/emergency/fragment/AedInfoFragment.kt
@@ -1,5 +1,7 @@
 package kr.tekit.lion.daongil.presentation.emergency.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.FragmentAedInfoBinding
+import kr.tekit.lion.daongil.domain.model.EmergencyBottom
 
 
 class AedInfoFragment : Fragment(R.layout.fragment_aed_info) {
@@ -18,6 +21,38 @@ class AedInfoFragment : Fragment(R.layout.fragment_aed_info) {
 
         binding.toolbarAedInfo.setNavigationOnClickListener {
             requireActivity().finish()
+        }
+
+        val data = requireActivity().intent.getSerializableExtra("data") as? EmergencyBottom
+
+        binding.aedName.text = data?.aedList?.aedName
+        binding.aedAddress.text = data?.aedList?.aedAdress
+        binding.aedLocationText.text = data?.aedList?.aedLocation
+        binding.aedMonTime.text = data?.aedList?.monTime
+        binding.aedTueTime.text = data?.aedList?.tueTime
+        binding.aedWedTime.text = data?.aedList?.wedTime
+        binding.aedThuTime.text = data?.aedList?.thuTime
+        binding.aedFriTime.text = data?.aedList?.friTime
+        binding.aedSatTime.text = data?.aedList?.satTime
+        binding.aedSunTime.text = data?.aedList?.sunTime
+        binding.aedHolTime.text = data?.aedList?.holTime
+
+        binding.aedSunWeekText.text = "일요일은 ${data?.aedList?.sunAvailable}에 사용이 가능합니다."
+
+        binding.emergencyCall.setOnClickListener {
+            val phoneNumber = data?.aedList?.aedTel
+            if (!phoneNumber.isNullOrBlank()) {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(intent)
+            }
+        }
+
+        binding.mainEmergencyCall.setOnClickListener {
+            val phoneNumber = data?.aedList?.managerTel
+            if (!phoneNumber.isNullOrBlank()) {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(intent)
+            }
         }
     }
 }

d829c69c741e58541a6aeccb8d83b4fc43967781
rmadmsjm <dmswjdals12@naver.com>
2024-06-09 17:42:42 +0900

    [Feat] #103 여행지 후기 수정

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myreview/adapter/MyReviewModifyImageRVAdapter.kt
@@ -0,0 +1,41 @@
+package kr.tekit.lion.daongil.presentation.myreview.adapter
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kr.tekit.lion.daongil.R
+import kr.tekit.lion.daongil.databinding.ItemReviewWriteImageBinding
+
+class MyReviewModifyImageRVAdapter(private val imageList : MutableList<Uri>) : RecyclerView.Adapter<MyReviewModifyImageRVAdapter.MyReviewModifyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyReviewModifyViewHolder {
+        val binding : ItemReviewWriteImageBinding = ItemReviewWriteImageBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+
+        return MyReviewModifyViewHolder(binding, this)
+    }
+
+    override fun getItemCount(): Int  = imageList.size
+
+    override fun onBindViewHolder(holder: MyReviewModifyViewHolder, position: Int) {
+        holder.bind(imageList[position], position)
+    }
+
+    class MyReviewModifyViewHolder(val binding: ItemReviewWriteImageBinding, private val adapter : MyReviewModifyImageRVAdapter) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(image : Uri, position: Int) {
+            Glide.with(binding.itemWriteReviewImage.context)
+                .load(image)
+                .error(R.drawable.empty_view_small)
+                .into(binding.itemWriteReviewImage)
+
+            binding.itemWriteReviewCancelIb.setOnClickListener {
+                if (position != RecyclerView.NO_POSITION) {
+                    adapter.imageList.removeAt(position)
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+}

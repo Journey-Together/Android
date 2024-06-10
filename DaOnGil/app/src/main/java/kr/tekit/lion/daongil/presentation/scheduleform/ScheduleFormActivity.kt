53ca76a505b49ad6c72b71cf2c50c060fb3eca21
Boram <dev.lboram@gmail.com>
2024-06-10 17:48:06 +0900

    [Del] #123 Toolbar 제거

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/ScheduleFormActivity.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/ScheduleFormActivity.kt
@@ -3,9 +3,6 @@ package kr.tekit.lion.daongil.presentation.scheduleform
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.ActivityScheduleFormBinding
 import kr.tekit.lion.daongil.presentation.scheduleform.vm.ScheduleFormViewModel
 
@@ -19,25 +16,5 @@ class ScheduleFormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentScheduleForm) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val toolbar = binding.toolbarAddSchedule
-        setSupportActionBar(toolbar)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-
-        toolbar.setNavigationIcon(R.drawable.back_icon)
-        toolbar.setNavigationOnClickListener {
-            finish()
-        }
-
-    }
-
-    // 뒤로가기 버튼
-    override fun onSupportNavigateUp(): Boolean {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentScheduleForm) as NavHostFragment
-        val navController = navHostFragment.navController
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }

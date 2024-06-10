6d6ef46aabf19e836ab717f6bf7c7e3fc18eb91c
chanho <uni070@naver.com>
2024-06-10 18:08:28 +0900

    [Refactor] Develop Merge 누락 해결

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/onboarding/fragment/SplashFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/onboarding/fragment/SplashFragment.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.presentation.ext.repeatOnViewStarted
-import kr.tekit.lion.daongil.presentation.login.UiState
+import kr.tekit.lion.daongil.presentation.login.LogInState
 import kr.tekit.lion.daongil.presentation.onboarding.vm.SplashViewModel
 import kr.tekit.lion.daongil.presentation.onboarding.vm.SplashViewModelFactory
 import kr.tekit.lion.daongil.presentation.main.MainActivity
@@ -28,7 +26,7 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
                         startActivity(Intent(requireContext(), MainActivity::class.java))
                         requireActivity().finish()
                     }
-                    is UiState.LoginRequired -> {
+                    is LogInState.LoginRequired -> {
                         startActivity(Intent(requireContext(), OnBoardingActivity::class.java))
                     }
                     is LogInState.Checking -> {

570bae0b9fed87b69b24cc2b27651ef7264f9f95
chanho <uni070@naver.com>
2024-06-11 03:22:45 +0900

    [Fix] #139 테마 변경 버그 수정

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/onboarding/SplashActivity.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/onboarding/SplashActivity.kt
@@ -1,21 +1,44 @@
 package kr.tekit.lion.daongil.presentation.onboarding
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import kr.tekit.lion.daongil.R
-import kr.tekit.lion.daongil.databinding.ActivityOnBoardingBinding
 import kr.tekit.lion.daongil.databinding.ActivitySplashBinding
+import kr.tekit.lion.daongil.presentation.ext.repeatOnStarted
+import kr.tekit.lion.daongil.presentation.login.LogInState
+import kr.tekit.lion.daongil.presentation.main.MainActivity
+import kr.tekit.lion.daongil.presentation.onboarding.vm.SplashViewModel
+import kr.tekit.lion.daongil.presentation.onboarding.vm.SplashViewModelFactory
 
 class SplashActivity : AppCompatActivity() {
+    private val viewModel: SplashViewModel by viewModels { SplashViewModelFactory(this) }
 
-    private val binding : ActivitySplashBinding by lazy {
+    private val binding: ActivitySplashBinding by lazy {
         ActivitySplashBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        repeatOnStarted {
+            viewModel.uiState.collect { state ->
+                when (state) {
+                    is LogInState.LoggedIn -> {
+                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                        finish()
+                    }
+
+                    is LogInState.LoginRequired -> {
+                        startActivity(Intent(this@SplashActivity, OnBoardingActivity::class.java))
+                    }
+
+                    is LogInState.Checking -> {
+                        return@collect
+                    }
+                }
+            }
+        }
     }
 }

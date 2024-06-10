29f925409c2995deef8a219db8f1b432232bbf79
Boram <dev.lboram@gmail.com>
2024-06-11 02:53:13 +0900

    [Refactor] #123 여행일정 목록 Adapter 리팩토링 (불필요한 속성 제거)

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/adapter/FormScheduleAdapter.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/adapter/FormScheduleAdapter.kt
@@ -1,34 +1,30 @@
 package kr.tekit.lion.daongil.presentation.scheduleform.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.navigation.NavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kr.tekit.lion.daongil.databinding.ItemFormScheduleBinding
 import kr.tekit.lion.daongil.domain.model.DailySchedule
-import kr.tekit.lion.daongil.presentation.scheduleform.fragment.ScheduleDetailsFormFragmentDirections
-import kr.tekit.lion.daongil.presentation.scheduleform.vm.ScheduleFormViewModel
 
 class FormScheduleAdapter(
     private val dailyScheduleList: List<DailySchedule>,
-    private val context: Context,
-    private val navController: NavController,
-    private val scheduleViewModel: ScheduleFormViewModel
+    private val onAddButtonClickListener : (schedulePosition: Int) -> Unit,
+    private val onRemoveButtonClickListener : (schedulePosition: Int, placePosition: Int) -> Unit
 ) : RecyclerView.Adapter<FormScheduleAdapter.FormScheduleViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FormScheduleViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return FormScheduleViewHolder(
             ItemFormScheduleBinding.inflate(inflater, parent, false),
-            context,
-            navController,
-            scheduleViewModel
+            onAddItemClick = { schedulePosition ->
+                // '여행지 추가' 버튼 클릭리스너 -> 다음 화면으로 이동시켜준다. (스케쥴 position 전달)
+                this.onAddButtonClickListener(schedulePosition)
+            },
+            onRemoveButtonClickListener
         )
     }
 
     override fun onBindViewHolder(holder: FormScheduleViewHolder, position: Int) {
-        holder.bind(dailyScheduleList[position], position)
+        holder.bind(dailyScheduleList[position])
     }
 
     override fun getItemCount(): Int {
@@ -37,25 +33,19 @@ class FormScheduleAdapter(
 
     class FormScheduleViewHolder(
         private val binding: ItemFormScheduleBinding,
-        private val context: Context,
-        private val navController: NavController,
-        private val scheduleViewModel: ScheduleFormViewModel
+        private val onAddItemClick : (schedulePosition: Int) -> Unit,
+        private val onRemoveButtonClickListener : (schedulePosition: Int, placePosition: Int) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(dailySchedule: DailySchedule, schedulePosition: Int) {
+        fun bind(dailySchedule: DailySchedule) {
             binding.textViewFScheduleDate.text = dailySchedule.dailyDate
 
-            val formPlaceAdapter =
-                FormPlaceAdapter(dailySchedule.dailyPlaces, scheduleViewModel, schedulePosition)
+            // 추가할 여행지목록 Adapter
+            val formPlaceAdapter = FormPlaceAdapter(dailySchedule.dailyPlaces, layoutPosition, onRemoveButtonClickListener)
             binding.recyclerViewFSchedulePlaces.adapter = formPlaceAdapter
-            binding.recyclerViewFSchedulePlaces.layoutManager = LinearLayoutManager(context)
 
+            // 여행지 추가 버튼
             binding.buttonFScheduleAddPlace.setOnClickListener {
-                // 몇 번째 일정에 여행지를 추가하는지 파악하기 위해 schedulePosition 을 전달해준다.
-                val action =
-                    ScheduleDetailsFormFragmentDirections.actionScheduleDetailsFormFragmentToFormSearchFragment(
-                        schedulePosition
-                    )
-                navController.navigate(action)
+                onAddItemClick(layoutPosition)
             }
         }
     }

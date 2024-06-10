795e17471baef11b85f1f13433e68c02119f9889
Jejun Min <alswpwns11@naver.com>
2024-06-10 14:34:41 +0900

    [Refactor] #92 스플래쉬 화면 순서를 위한 파일 위치 및 코드 리펙토링

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/onboarding/fragment/SplashFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/onboarding/fragment/SplashFragment.kt
@@ -1,17 +1,19 @@
-package kr.tekit.lion.daongil.presentation.login.fragment
+package kr.tekit.lion.daongil.presentation.onboarding.fragment
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.presentation.ext.repeatOnViewStarted
 import kr.tekit.lion.daongil.presentation.login.UiState
-import kr.tekit.lion.daongil.presentation.login.vm.SplashViewModel
-import kr.tekit.lion.daongil.presentation.login.vm.SplashViewModelFactory
+import kr.tekit.lion.daongil.presentation.onboarding.vm.SplashViewModel
+import kr.tekit.lion.daongil.presentation.onboarding.vm.SplashViewModelFactory
 import kr.tekit.lion.daongil.presentation.main.MainActivity
+import kr.tekit.lion.daongil.presentation.onboarding.OnBoardingActivity
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
     private val viewModel: SplashViewModel by viewModels { SplashViewModelFactory(requireContext()) }
@@ -24,9 +26,10 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
                 when(state){
                     is UiState.LoggedIn ->{
                         startActivity(Intent(requireContext(), MainActivity::class.java))
+                        requireActivity().finish()
                     }
                     is UiState.LoginRequired -> {
-                        Navigation.findNavController(view).navigate(R.id.to_loginFragment)
+                        startActivity(Intent(requireContext(), OnBoardingActivity::class.java))
                     }
                     is UiState.Checking -> {
                         return@collect

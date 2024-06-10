fc4c721897657ca742c1b125a02226d087925b5f
Jieun <lje2000lje@naver.com>
2024-06-10 15:17:45 +0900

    [Add] #104 PlaceMainInfo, AroundPlace, RecommendPlace 모델 추가

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/AroundPlace.kt
@@ -0,0 +1,10 @@
+package kr.tekit.lion.daongil.domain.model
+
+
+data class AroundPlace (
+    val address: String,
+    val disability: List<String>,
+    val image: String,
+    val name: String,
+    val placeId: Int
+)

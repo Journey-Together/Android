23a8a02d5b838a92f38bdf17ccaa2201e939fa10
Jieun <lje2000lje@naver.com>
2024-06-10 15:22:05 +0900

    [Feat] #104 홈 메인화면 ViewModel, ViewModelFactory 생성

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModel.kt
@@ -1,11 +1,41 @@
 package kr.tekit.lion.daongil.presentation.main.vm
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import kr.tekit.lion.daongil.domain.usecase.areacode.InitAreaCodeInfoUseCase
+import kr.tekit.lion.daongil.domain.model.AroundPlace
+import kr.tekit.lion.daongil.domain.model.RecommendPlace
+import kr.tekit.lion.daongil.domain.usecase.GetPlaceMainInfoUseCase
+import kr.tekit.lion.daongil.domain.usecase.areacode.GetAreaCodeInfoUseCase
+import kr.tekit.lion.daongil.domain.usecase.base.onError
+import kr.tekit.lion.daongil.domain.usecase.base.onSuccess
 
 class MainViewModel(
+    private val getAreaCodeInfoUseCase: GetAreaCodeInfoUseCase,
+    private val getPlaceMainInfoUseCase: GetPlaceMainInfoUseCase
 ) : ViewModel() {
+
+    private val _aroundPlaceInfo = MutableLiveData<List<AroundPlace>>()
+    val aroundPlaceInfo : LiveData<List<AroundPlace>> = _aroundPlaceInfo
+
+    private val _recommendPlaceInfo = MutableLiveData<List<RecommendPlace>>()
+    val recommendPlaceInfo : LiveData<List<RecommendPlace>> = _recommendPlaceInfo
+    fun getAreaCode(area: String) = viewModelScope.launch {
+        getAreaCodeInfoUseCase(area)
+    }
+
+    fun getPlaceMain(areaCode: String, sigunguCode: String) = viewModelScope.launch {
+        getPlaceMainInfoUseCase(areaCode, sigunguCode).onSuccess {
+            Log.d("getPlaceMain", it.toString())
+            _aroundPlaceInfo.value = it.aroundPlaceList
+            _recommendPlaceInfo.value = it.recommendPlaceList
+
+        }.onError {
+            Log.d("getPlaceMain", it.toString())
+        }
+    }
 
 }
--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModelFactory.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/main/vm/MainViewModelFactory.kt
@@ -3,13 +3,23 @@ package kr.tekit.lion.daongil.presentation.main.vm
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kr.tekit.lion.daongil.domain.repository.AreaCodeRepository
+import kr.tekit.lion.daongil.domain.repository.PlaceMainInfoRepository
+import kr.tekit.lion.daongil.domain.usecase.GetPlaceMainInfoUseCase
+import kr.tekit.lion.daongil.domain.usecase.areacode.GetAreaCodeInfoUseCase
 
 class MainViewModelFactory(context: Context) : ViewModelProvider.Factory {
+    private val areaCodeRepository = AreaCodeRepository.create(context)
+    private val placeMainInfoRepository = PlaceMainInfoRepository.create()
+
+    private val getAreaCodeInfoUseCase = GetAreaCodeInfoUseCase(areaCodeRepository)
+    private val getPlaceMainInfoUseCase = GetPlaceMainInfoUseCase(placeMainInfoRepository)
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(
-
+                getAreaCodeInfoUseCase,
+                getPlaceMainInfoUseCase
             ) as T
         }
         throw IllegalArgumentException("unknown ViewModel class")

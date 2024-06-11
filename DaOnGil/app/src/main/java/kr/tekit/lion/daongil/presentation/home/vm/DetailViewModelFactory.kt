c7e2783348608fad95985df9341827ff2e255d65
Jieun <lje2000lje@naver.com>
2024-06-12 03:00:34 +0900

    [Feat] #158 관광지 상세보기 게스트 버전 로그인 유무 확인, 게시글 조회, 북마크 업데이트 api 연결

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/home/vm/DetailViewModelFactory.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/home/vm/DetailViewModelFactory.kt
@@ -1,19 +1,33 @@
 package kr.tekit.lion.daongil.presentation.home.vm
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kr.tekit.lion.daongil.domain.repository.AuthRepository
+import kr.tekit.lion.daongil.domain.repository.BookmarkRepository
+import kr.tekit.lion.daongil.domain.repository.PlaceDetailInfoGuestRepository
 import kr.tekit.lion.daongil.domain.repository.PlaceDetailInfoRepository
+import kr.tekit.lion.daongil.domain.usecase.GetPlaceDetailInfoGuestUseCase
 import kr.tekit.lion.daongil.domain.usecase.GetPlaceDetailInfoUseCase
+import kr.tekit.lion.daongil.domain.usecase.UpdatePlaceBookmarkUseCase
 
-class DetailViewModelFactory : ViewModelProvider.Factory{
+class DetailViewModelFactory(context: Context) : ViewModelProvider.Factory{
+    private val authRepository = AuthRepository.create(context)
     private val placeDetailInfoRepository = PlaceDetailInfoRepository.crate()
+    private val placeDetailInfoGuestRepository = PlaceDetailInfoGuestRepository.create()
+    private val bookmarkRepository = BookmarkRepository.create()
 
     private val getPlaceDetailInfoUseCase = GetPlaceDetailInfoUseCase(placeDetailInfoRepository)
+    private val getPlaceDetailInfoGuestUseCase = GetPlaceDetailInfoGuestUseCase(placeDetailInfoGuestRepository)
+    private val updatePlaceBookmarkUseCase = UpdatePlaceBookmarkUseCase(bookmarkRepository)
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
             return DetailViewModel(
-                getPlaceDetailInfoUseCase
+                authRepository,
+                getPlaceDetailInfoUseCase,
+                getPlaceDetailInfoGuestUseCase,
+                updatePlaceBookmarkUseCase
             ) as T
         }
         throw IllegalArgumentException("unknown ViewModel class")

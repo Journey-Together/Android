f0950df9e28c4bab5e2cd7ed5b234dcd9b38c2f8
Jieun <lje2000lje@naver.com>
2024-06-12 02:57:28 +0900

    [Feat] #158 관광지 상세보기 게스트 버전 Response 데이터 생성

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/detailPlaceGuest/Data.kt
@@ -0,0 +1,22 @@
+package kr.tekit.lion.daongil.data.dto.remote.response.detailPlaceGuest
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Data(
+    val address: String,
+    val bookmarkNum: Int,
+    val category: String,
+    val disability: List<Int>,
+    @Json(name = "imgae")
+    val image: String,
+    val mapX: String,
+    val mapY: String,
+    val name: String,
+    val overview: String,
+    val placeId: Int,
+    val reviewList: List<ReviewGuestRes>?,
+    val subDisability: List<String>
+)

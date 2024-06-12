ce2d60aeb1bfab2258423d28f53463ad810e2f23
yein <inwon9892@naver.com>
2024-06-12 15:58:09 +0900

    [Feat] #167 내 일정보기 API 연결

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/MyMainSchedule.kt
@@ -0,0 +1,12 @@
+package kr.tekit.lion.daongil.domain.model
+
+
+data class MyMainSchedule(
+    val endDate: String?,
+    val hasReview: Boolean?,
+    val imageUrl: String?,
+    val planId: Long?,
+    val remainDate: String?,
+    val startDate: String?,
+    val title: String?
+)
--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/repository/PlanRepository.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/repository/PlanRepository.kt
@@ -4,6 +4,7 @@ import kr.tekit.lion.daongil.data.datasource.PlanDataSource
 import kr.tekit.lion.daongil.data.network.RetrofitInstance
 import kr.tekit.lion.daongil.data.network.service.PlanService
 import kr.tekit.lion.daongil.data.repository.PlanRepositoryImpl
+import kr.tekit.lion.daongil.domain.model.MyMainSchedule
 import kr.tekit.lion.daongil.domain.model.NewPlan
 import kr.tekit.lion.daongil.domain.model.OpenPlan
 import kr.tekit.lion.daongil.domain.model.PlaceSearchResult
@@ -15,6 +16,8 @@ interface PlanRepository {
 
     // 구현해야 할 메서드
     suspend fun getPlaceSearchResult(word: String, page: Int, size: Int) : PlaceSearchResult
+
+    suspend fun getMyMainSchedule(): List<MyMainSchedule?>?
 
     companion object{
         fun create(): PlanRepositoryImpl{

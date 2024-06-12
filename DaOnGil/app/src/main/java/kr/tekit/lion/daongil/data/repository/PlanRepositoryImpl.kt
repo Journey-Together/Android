d07dc19c61f38d530ee22d30daad53dda3906148
Boram <dev.lboram@gmail.com>
2024-06-12 11:32:02 +0900

    [Feat] #150 여행일정 작성 기능 RepositoryImpl에 구현

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/repository/PlanRepositoryImpl.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/repository/PlanRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package kr.tekit.lion.daongil.data.repository
 
 import kr.tekit.lion.daongil.data.datasource.PlanDataSource
+import kr.tekit.lion.daongil.data.dto.remote.request.toRequestBody
+import kr.tekit.lion.daongil.domain.model.NewPlan
 import kr.tekit.lion.daongil.domain.model.OpenPlan
 import kr.tekit.lion.daongil.domain.repository.PlanRepository
 
@@ -9,5 +11,9 @@ class PlanRepositoryImpl(
 ) : PlanRepository {
     override suspend fun getOpenPlanList(size: Int, page: Int): OpenPlan {
         return planDataSource.getOpenPlanList(size, page).toDomainModel()
+    }
+
+    override suspend fun addNewPlan(request: NewPlan) {
+        return planDataSource.addNewPlan(request.toRequestBody())
     }
 }

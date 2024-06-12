49a0b99cf28b086c00522ef70f5a365313ba01fb
Boram <dev.lboram@gmail.com>
2024-06-12 13:11:39 +0900

    [Del] #150 중복 파일 삭제

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/bookmark/BookmarkNameData.kt
+++
@@ -1,9 +0,0 @@
-package kr.tekit.lion.daongil.data.dto.remote.response.bookmark
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class BookmarkNameData(
-    val placeId : Long,
-    val placeName : String
-)

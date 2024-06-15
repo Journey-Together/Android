4273450f9becacc77cb9bd08e7e6be179cdcd71f
unknown <chanho0908>
2024-06-15 23:38:55 +0900

    [Feat] #184 리스트, 지도 검색 Domain Model

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/ListSearchOption.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/ListSearchOption.kt
@@ -4,16 +4,12 @@ import java.util.TreeSet
 
 data class ListSearchOption (
     val category: String,
-    val query: String,
     val size: Int,
     val page: Int,
-    val minX: Double? = 0.0,
-    val maxX: Double?= 0.0,
-    val minY: Double?= 0.0,
-    val maxY: Double?= 0.0,
-    val disabilityType: TreeSet<String> = TreeSet(),
-    val detailFilter: Set<String> = TreeSet(),
-    val areaCode: String ?= "",
-    val sigunguCode: String? = "",
-    val arrange: String = "C"
+    val query: String? = null,
+    val disabilityType: TreeSet<String>? = null,
+    val detailFilter: Set<String>? = null,
+    val areaCode: String? = null,
+    val sigunguCode: String? = null,
+    val arrange: String = "B"
 )
--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/MapSearchResult.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/MapSearchResult.kt
@@ -1,4 +1,11 @@
 package kr.tekit.lion.daongil.domain.model
 
-class MapSearchResult {
-}
+data class MapSearchResult (
+    val address: String,
+    val name: String,
+    val image: String,
+    val disability: List<Int>,
+    val placeId: Int,
+    val latitude: Double,
+    val longitude: Double
+)

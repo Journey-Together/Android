4b58cd63c84c91e971ff5c783267c685e21aabd8
rmadmsjm <dmswjdals12@naver.com>
2024-06-18 22:18:31 +0900

    [Feat] #190 마이페이지 관심 유형 viewModel

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/concerntype/vm/ConcernTypeViewModel.kt
@@ -0,0 +1,53 @@
+package kr.tekit.lion.daongil.presentation.concerntype.vm
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kr.tekit.lion.daongil.domain.model.ConcernType
+import kr.tekit.lion.daongil.domain.usecase.base.onError
+import kr.tekit.lion.daongil.domain.usecase.base.onSuccess
+import kr.tekit.lion.daongil.domain.usecase.myinfo.GetConcernTypeUseCase
+import kr.tekit.lion.daongil.domain.usecase.myinfo.UpdateConcernTypeUseCase
+
+class ConcernTypeViewModel(
+    private val getConcernTypeUseCase: GetConcernTypeUseCase,
+    private val updateConcernTypeUseCase: UpdateConcernTypeUseCase
+) : ViewModel() {
+
+    private val _concernType = MutableLiveData<ConcernType>()
+    val concernType: LiveData<ConcernType> = _concernType
+
+    init {
+        getConcernType()
+    }
+
+    fun getConcernType() {
+        viewModelScope.launch {
+            getConcernTypeUseCase().onSuccess {
+                _concernType.value = it
+            }.onError {
+                Log.d("getConcernType", it.toString())
+            }
+        }
+    }
+
+    fun updateConcernType(requestBody: ConcernType) {
+        viewModelScope.launch {
+            updateConcernTypeUseCase(requestBody).onSuccess {
+            }.onError {
+                Log.d("updateConcernType", it.toString())
+            }
+        }
+
+        _concernType.value = _concernType.value?.copy(
+            isPhysical = requestBody.isPhysical,
+            isVisual = requestBody.isVisual,
+            isHear = requestBody.isHear,
+            isChild = requestBody.isChild,
+            isElderly = requestBody.isElderly
+        )
+    }
+}

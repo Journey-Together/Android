2bbb44077825c6775c6bac976f98ad04ac4df258
rmadmsjm <dmswjdals12@naver.com>
2024-06-22 19:46:24 +0900

    [Remove] #193 MyReview.kt 삭제

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/MyReview.kt
+++
@@ -1,8 +0,0 @@
-package kr.tekit.lion.daongil.domain.model
-
-data class MyReview(
-    val myReviewLocation : String,
-    val myReviewLocationName : String,
-    val myReviewImage : String?,
-    val rating : Double
-)

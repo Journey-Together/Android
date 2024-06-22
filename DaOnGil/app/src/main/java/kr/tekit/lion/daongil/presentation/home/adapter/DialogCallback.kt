113c970bb75798068235377651f19df1f7dcfcd4
Jieun <lje2000lje@naver.com>
2024-06-23 04:33:16 +0900

    [Remove] #187 DialogCallback 인터페이스 삭제

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/home/adapter/DialogCallback.kt
+++
@@ -1,7 +0,0 @@
-package kr.tekit.lion.daongil.presentation.home.adapter
-
-import kr.tekit.lion.daongil.presentation.main.dialog.ConfirmDialogInterface
-
-interface DialogCallback {
-    fun showConfirmDialog(dialogInterface: ConfirmDialogInterface)
-}

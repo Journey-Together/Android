0cca98ae37a3465064c9e4459e4a2b9410e60de0
Jieun <lje2000lje@naver.com>
2024-06-23 02:35:51 +0900

    [Feat] #187 리뷰 모아보기 Response 데이터 클래스 생성

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/placeReview/PlaceReviewResponse.kt
@@ -0,0 +1,33 @@
+package kr.tekit.lion.daongil.data.dto.remote.response.placeReview
+
+import com.squareup.moshi.JsonClass
+import kr.tekit.lion.daongil.domain.model.PlaceReview
+import kr.tekit.lion.daongil.domain.model.PlaceReviewInfo
+
+@JsonClass(generateAdapter = true)
+data class PlaceReviewResponse(
+    val code: Int,
+    val data: Data,
+    val message: String
+) {
+    fun toDomainModel(): PlaceReviewInfo {
+        return PlaceReviewInfo(
+            placeReviewList = data.myPlaceReviewList.map {
+                PlaceReview(
+                    content = it.content,
+                    date = it.date,
+                    grade = it.grade,
+                    imageList = it.imageList,
+                    nickname = it.nickname,
+                    profileImg = it.profileImg,
+                    reviewId = it.reviewId
+                )
+            },
+            pageNo = data.pageNo,
+            pageSize = data.pageSize,
+            placeAddress = data.placeAddress,
+            placeName = data.placeName,
+            totalPages = data.totalPages
+        )
+    }
+}

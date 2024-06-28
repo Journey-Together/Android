dbcc85ad5915672e84176a968c8930a54a10c11a
Boram <dev.lboram@gmail.com>
2024-06-28 15:18:35 +0900

    [Feat] #197 PlanRepository에 일정 후기 작성 API 추가

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/repository/PlanRepository.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/repository/PlanRepository.kt
@@ -7,8 +7,10 @@ import kr.tekit.lion.daongil.data.repository.PlanRepositoryImpl
 import kr.tekit.lion.daongil.domain.model.BriefScheduleInfo
 import kr.tekit.lion.daongil.domain.model.MyMainSchedule
 import kr.tekit.lion.daongil.domain.model.NewPlan
+import kr.tekit.lion.daongil.domain.model.NewScheduleReview
 import kr.tekit.lion.daongil.domain.model.OpenPlan
 import kr.tekit.lion.daongil.domain.model.PlaceSearchResult
+import kr.tekit.lion.daongil.domain.model.ReviewImg
 
 interface PlanRepository {
     suspend fun getOpenPlanList(size: Int, page: Int): OpenPlan
@@ -21,6 +23,12 @@ interface PlanRepository {
     suspend fun getMyMainSchedule(): List<MyMainSchedule?>?
 
     suspend fun getBriefScheduleInfo(planId: Long) : BriefScheduleInfo
+
+    suspend fun addNewScheduleReview(
+        planId: Long,
+        scheduleReview: NewScheduleReview,
+        images: List<ReviewImg>
+    )
 
     companion object{
         fun create(): PlanRepositoryImpl{

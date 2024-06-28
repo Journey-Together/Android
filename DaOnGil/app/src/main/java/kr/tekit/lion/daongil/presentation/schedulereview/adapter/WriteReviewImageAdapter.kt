dd98ddf4077c5f2704c745c2c723943f6e35ad5d
Boram <dev.lboram@gmail.com>
2024-06-28 15:16:06 +0900

    [Feat] #197 일정 후기 이미지목록 Adapter 추가

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedulereview/adapter/WriteReviewImageAdapter.kt
@@ -0,0 +1,50 @@
+package kr.tekit.lion.daongil.presentation.schedulereview.adapter
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kr.tekit.lion.daongil.R
+import kr.tekit.lion.daongil.databinding.ItemReviewWriteImageBinding
+
+class WriteReviewImageAdapter(
+    private val images : MutableList<Uri>,
+    private val imageRemoveListener: (imagePosition: Int) -> Unit
+) : RecyclerView.Adapter<WriteReviewImageAdapter.WriteScheduleReviewImageViewHolder>(){
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): WriteScheduleReviewImageViewHolder {
+        val binding: ItemReviewWriteImageBinding = ItemReviewWriteImageBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+
+        return WriteScheduleReviewImageViewHolder(binding, imageRemoveListener)
+    }
+
+    override fun onBindViewHolder(holder: WriteScheduleReviewImageViewHolder, position: Int) {
+        holder.bind(images[position])
+    }
+
+    override fun getItemCount(): Int = images.size
+
+    class WriteScheduleReviewImageViewHolder(
+        private val binding: ItemReviewWriteImageBinding,
+        private val imageRemoveListener: (imagePosition: Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root){
+        init {
+            binding.itemWriteReviewCancelIb.setOnClickListener {
+                imageRemoveListener(absoluteAdapterPosition)
+            }
+        }
+
+        fun bind(image: Uri){
+            Glide.with(binding.itemWriteReviewImage.context)
+                .load(image)
+                .error(R.drawable.empty_view_small)
+                .into(binding.itemWriteReviewImage)
+        }
+    }
+}

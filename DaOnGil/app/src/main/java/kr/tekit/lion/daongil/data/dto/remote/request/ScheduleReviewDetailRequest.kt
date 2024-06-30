fe8e14c125a4eb61e914b05349da86cdcfac33d7
Boram <dev.lboram@gmail.com>
2024-06-30 18:22:03 +0900

    [Remove] #197 코드 수정하면서 불필요해진 파일 삭제

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/request/ScheduleReviewDetailRequest.kt
+++
@@ -1,7 +0,0 @@
-package kr.tekit.lion.daongil.data.dto.remote.request
-
-data class ScheduleReviewDetailRequest(
-    val grade: Float,
-    val content: String,
-    val isPublic: Boolean
-)
--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/NewScheduleReviewDetail.kt
+++
@@ -1,7 +0,0 @@
-package kr.tekit.lion.daongil.domain.model
-
-data class NewScheduleReviewDetail (
-    val grade: Float,
-    val content: String,
-    val isPublic: Boolean
-)

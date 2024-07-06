0d8e7b07d3a499363e1968427858f871340823d0
rmadmsjm <dmswjdals12@naver.com>
2024-07-07 04:53:22 +0900

    [Refactor] #211 MyPlaceReviewDto MyPlaceReviewDto 구조 변경

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/myreview/MyPlaceReviewDto.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/myreview/MyPlaceReviewDto.kt
@@ -1,12 +1,15 @@
 package kr.tekit.lion.daongil.data.dto.remote.response.myreview
 
 import com.squareup.moshi.JsonClass
+import java.time.LocalDate
 
 @JsonClass(generateAdapter = true)
 data class MyPlaceReviewDto(
-    val address: String,
+    val content: String,
+    val date: LocalDate,
     val grade: Float,
-    val image: String,
+    val images: List<String>,
     val name: String,
+    val placeId: Long,
     val reviewId: Long
 )

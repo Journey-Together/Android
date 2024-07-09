59b819fee571a84b2e8113354b75800e5daa838e
Jieun <lje2000lje@naver.com>
2024-07-10 00:36:12 +0900

    [Feat] #212 뷰모델 페이징 처리 관련 함수 추가

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/home/vm/ReviewListViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/home/vm/ReviewListViewModel.kt
@@ -17,12 +17,46 @@ class ReviewListViewModel(
     private val _placeReviewInfo = MutableLiveData<PlaceReviewInfo>()
     val placeReviewInfo : LiveData<PlaceReviewInfo> = _placeReviewInfo
 
-    fun getPlaceReview(placeId: Long, size: Int, page: Int) = viewModelScope.launch {
+    private val _isLastPage = MutableLiveData<Boolean>()
+    val isLastPage : LiveData<Boolean> = _isLastPage
+
+    private var placeId : Long = 0
+
+    init {
+        _isLastPage.value = false
+    }
+
+    fun setPlaceId(placeId: Long) {
+        this.placeId = placeId
+        getPlaceReview(placeId, 5, 0)
+    }
+
+    private fun getPlaceReview(placeId: Long, size: Int, page: Int) = viewModelScope.launch {
         getPlaceReviewListUseCase(placeId, size, page).onSuccess {
             Log.d("getPlaceReview", it.toString())
             _placeReviewInfo.value = it
         }.onError {
             Log.d("getPlaceReview", it.toString())
+        }
+    }
+
+    fun getNewPlaceReview(placeId: Long, size: Int) = viewModelScope.launch {
+        val page = _placeReviewInfo.value?.pageNo
+
+        if (page != null) {
+            getPlaceReviewListUseCase(placeId, size = size, page = page + 1).onSuccess {
+                if (it.pageNo == it.totalPages) {
+                    _isLastPage.value = true
+                } else {
+                    val reviews = _placeReviewInfo.value?.placeReviewList ?: emptyList()
+                    val newReviews = reviews + it.placeReviewList
+                    val newReviewData = it.copy(placeReviewList = newReviews)
+
+                    _placeReviewInfo.value = newReviewData
+                }
+            }.onError {
+                Log.d("getNewPlaceReview", it.toString())
+            }
         }
     }
 }

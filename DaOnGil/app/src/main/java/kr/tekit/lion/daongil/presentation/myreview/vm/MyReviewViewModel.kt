94565e7d6897eb35620d6e2a1a644e544c9e255c
rmadmsjm <dmswjdals12@naver.com>
2024-07-09 23:52:50 +0900

    [Refactor] #220 페이징 처리 로직 개선

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myreview/vm/MyReviewViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myreview/vm/MyReviewViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.launch
 import kr.tekit.lion.daongil.domain.model.MyPlaceReview
 import kr.tekit.lion.daongil.domain.usecase.base.onError
 import kr.tekit.lion.daongil.domain.usecase.base.onSuccess
+import kr.tekit.lion.daongil.domain.usecase.place.DeleteMyPlaceReviewUseCase
 import kr.tekit.lion.daongil.domain.usecase.place.GetMyPlaceReviewUseCase
 
 class MyReviewViewModel(
@@ -21,6 +22,7 @@ class MyReviewViewModel(
     private val _isLastPage = MutableLiveData<Boolean>()
     val isLastPage: LiveData<Boolean> = _isLastPage
 
+    private var isRequesting = false
 
     init {
         _isLastPage.value = false
@@ -30,26 +32,36 @@ class MyReviewViewModel(
     private fun getMyPlaceReview(size: Int, page: Int) = viewModelScope.launch {
         getMyPlaceReviewUseCase(size, page).onSuccess {
             _myPlaceReview.value = it
+            _isLastPage.value = it.myPlaceReviewInfoList.size < size
         }.onError {
             Log.d("getMyPlaceReview", it.toString())
         }
     }
 
     fun getNextMyPlaceReview(size: Int) = viewModelScope.launch {
-        val page = _myPlaceReview.value?.pageNo
+        val page = _myPlaceReview.value?.pageNo ?:0
 
-        if (page != null) {
-            getMyPlaceReviewUseCase(size, page+1).onSuccess { newReviews ->
-                if (newReviews.myPlaceReviewInfoList.isEmpty()) {
+        if (_isLastPage.value == false && !isRequesting) {
+            isRequesting = true
+
+            getMyPlaceReviewUseCase(size, page + 1).onSuccess { newReviews ->
+                val currentReviews = _myPlaceReview.value?.myPlaceReviewInfoList ?: emptyList()
+                val updatedReviews = currentReviews + newReviews.myPlaceReviewInfoList
+                val updatedReviewData = newReviews.copy(myPlaceReviewInfoList = updatedReviews)
+                _myPlaceReview.value = updatedReviewData
+
+                if (newReviews.myPlaceReviewInfoList.size < size) {
                     _isLastPage.value = true
-                } else {
-                    val currentReviews = _myPlaceReview.value?.myPlaceReviewInfoList ?: emptyList()
-                    val updatedReviews = currentReviews + newReviews.myPlaceReviewInfoList
-                    val updatedReviewData = newReviews.copy(myPlaceReviewInfoList = updatedReviews)
-                    _myPlaceReview.value = updatedReviewData
                 }
+
+                Log.d("MyReviewViewModel", "currentReviews : ${currentReviews}")
+                Log.d("MyReviewViewModel", "updatedReviews : ${updatedReviews}")
+                Log.d("MyReviewViewModel", "updatedReviewData : ${updatedReviewData}")
+                Log.d("MyReviewViewModel", _isLastPage.value.toString())
             }.onError {
                 Log.d("getNextMyPlaceReview", it.toString())
+            }.also {
+                isRequesting = false
             }
         }
     }

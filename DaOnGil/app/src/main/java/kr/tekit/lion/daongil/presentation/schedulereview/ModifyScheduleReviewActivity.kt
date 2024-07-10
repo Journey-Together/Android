4986f3a73c4b99a4ddee59a953384ac43e30015c
Boram <dev.lboram@gmail.com>
2024-07-10 17:02:28 +0900

    [Feat] #217 일정 후기 수정 화면 - 기본 기능 구현

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedulereview/ModifyScheduleReviewActivity.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedulereview/ModifyScheduleReviewActivity.kt
@@ -2,24 +2,36 @@ package kr.tekit.lion.daongil.presentation.schedulereview
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.ext.SdkExtensions
 import android.provider.Settings
+import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.ActivityModifyScheduleReviewBinding
+import kr.tekit.lion.daongil.domain.model.ReviewImage
+import kr.tekit.lion.daongil.presentation.ext.toAbsolutePath
 import kr.tekit.lion.daongil.presentation.main.dialog.ConfirmDialog
 import kr.tekit.lion.daongil.presentation.main.dialog.ConfirmDialogInterface
+import kr.tekit.lion.daongil.presentation.schedulereview.adapter.ModifyReviewImageAdapter
+import kr.tekit.lion.daongil.presentation.schedulereview.vm.ModifyScheduleReviewViewModel
 
 class ModifyScheduleReviewActivity : AppCompatActivity(), ConfirmDialogInterface {
+
+    private val viewModel : ModifyScheduleReviewViewModel by viewModels()
 
     private val pickMedia =
         registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
             if (uri != null) {
-//                saveImageDataAndPath(uri)
+                saveImageDataAndPath(uri)
             }
         }
 
@@ -30,7 +42,7 @@ class ModifyScheduleReviewActivity : AppCompatActivity(), ConfirmDialogInterface
                 // 선택한 이미지의 Uri 가져오기
                 val uri = result.data?.data
                 uri?.let {
-//                    saveImageDataAndPath(uri)
+                    saveImageDataAndPath(uri)
                 }
             }
         }
@@ -57,14 +69,58 @@ class ModifyScheduleReviewActivity : AppCompatActivity(), ConfirmDialogInterface
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+//        val planId = intent.getLongExtra("planId", -1)
+
         initTestView()
+        initToolbar()
+        initView()
+        settingImageRVAdapter()
+        settingButtonClickListner()
     }
 
-    private fun initTestView(){
+    private fun initTestView() {
         binding.textViewModifyScheReviewName.text = "즐거운 여행"
         binding.textViewModifyScheReviewPeriod.text = "2024.07.01 - 2024.07.02"
         binding.textViewModifyScheReviewPhotoNum.text = getString(R.string.text_num_of_images, 0)
+    }
 
+    private fun initToolbar() {
+        binding.toolbarModifyScheReview.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initView() {
+        viewModel.numOfImages.observe(this@ModifyScheduleReviewActivity) { numOfImages ->
+            binding.textViewModifyScheReviewPhotoNum.text =
+                getString(R.string.text_num_of_images, numOfImages)
+        }
+    }
+
+    private fun settingImageRVAdapter() {
+        viewModel.imageList.observe(this) { imageList ->
+            val modifyReviewImageAdapter = ModifyReviewImageAdapter(imageList) { position ->
+                viewModel.removeReviewImageFromList(position)
+            }
+            binding.recyclerViewModifyScheReviewPhotos.adapter = modifyReviewImageAdapter
+        }
+    }
+
+    private fun settingButtonClickListner() {
+        binding.apply {
+            imageButtonModifyScheReviewPhotoAdd.setOnClickListener {
+                if (!viewModel.isMoreImageAttachable()) {
+                    showSnackBar(it, "사진은 최대 4개까지 첨부할 수 있습니다")
+                    return@setOnClickListener
+                }
+
+                if (isPhotoPickerAvailable()) {
+                    pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                } else {
+                    checkPermission()
+                }
+            }
+        }
     }
 
     private fun isPhotoPickerAvailable(): Boolean {
@@ -77,11 +133,27 @@ class ModifyScheduleReviewActivity : AppCompatActivity(), ConfirmDialogInterface
         }
     }
 
+    // 갤러리 접근 권한 확인 함수
+    private fun checkPermission() {
+        val permissionReadExternal = android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+        val permissionReadExternalGranted = ContextCompat.checkSelfPermission(
+            this,
+            permissionReadExternal
+        ) == PackageManager.PERMISSION_GRANTED
+
+        // 포토피커를 사용하지 못하는 버전만 권한 확인 (SDK 30 미만)
+        if (permissionReadExternalGranted) {
+            startAlbumLauncher()
+        } else {
+            permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
     private fun startAlbumLauncher() {
         val albumIntent = Intent(Intent.ACTION_GET_CONTENT)
         albumIntent.type = "image/*"  // 이미지 타입만 선택하도록 설정
-        // TO DO
-//        albumLauncher.launch(albumIntent)
+        albumLauncher.launch(albumIntent)
     }
 
     override fun onPosBtnClick() {
@@ -90,6 +162,20 @@ class ModifyScheduleReviewActivity : AppCompatActivity(), ConfirmDialogInterface
         val uri = Uri.fromParts("package", packageName, null)
         intent.data = uri
         startActivity(intent)
+    }
+
+    private fun saveImageDataAndPath(uri: Uri) {
+        val imagePath = toAbsolutePath(uri)
+        if (imagePath != null) {
+            val newImage = ReviewImage(imageUri = uri, uriPath = imagePath)
+            viewModel.addNewReviewImage(newImage)
+        }
+    }
+
+    private fun showSnackBar(view: View, message: String) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+            .setBackgroundTint(getColor(R.color.text_secondary))
+            .show()
     }
 
 }

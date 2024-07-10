f0de2e37b299455c93b5597ab271c721746b201d
Boram <dev.lboram@gmail.com>
2024-07-10 16:57:55 +0900

    [Feat] #217 후기 이미지 Adapter

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/domain/model/ReviewImage.kt
@@ -0,0 +1,9 @@
+package kr.tekit.lion.daongil.domain.model
+
+import android.net.Uri
+
+data class ReviewImage(
+    val imageUrl: String? = null,
+    val imageUri: Uri? = null,
+    val uriPath: String? = null
+)

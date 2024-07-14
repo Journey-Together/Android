0e42f7cd8fd63c93a6b818596bf9c3bcc95bc938
Boram <dev.lboram@gmail.com>
2024-07-15 02:27:22 +0900

    [Feat] #224 다녀온 일정 dto 추가

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/plan/myScheduleElapsed/MyElapsedResponse.kt
@@ -0,0 +1,31 @@
+package kr.tekit.lion.daongil.data.dto.remote.response.plan.myScheduleElapsed
+
+import com.squareup.moshi.JsonClass
+import kr.tekit.lion.daongil.domain.model.MyElapsedScheduleInfo
+import kr.tekit.lion.daongil.domain.model.MyElapsedSchedules
+
+@JsonClass(generateAdapter = true)
+data class MyElapsedResponse(
+    val code: Int,
+    val message: String,
+    val data: Data,
+){
+    fun toDomainModel(): MyElapsedSchedules {
+        return MyElapsedSchedules(
+            myElapsedScheduleList = data.planResList.map{
+                MyElapsedScheduleInfo(
+                    planId = it.planId,
+                    title = it.title,
+                    startDate = it.startDate,
+                    endDate = it.endDate,
+                    imageUrl = it.imageUrl ?: "",
+                    hasReview = it.hasReview
+                )
+            },
+            pageNo = data.pageNo,
+            pageSize = data.pageSize,
+            totalPages = data.totalPages,
+            last = data.last
+        )
+    }
+}

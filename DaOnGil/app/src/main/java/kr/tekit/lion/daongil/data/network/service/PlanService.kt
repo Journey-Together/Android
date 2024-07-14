744a6c01bd5585b5d58dda20cb9ac7c68aa4a80d
Boram <dev.lboram@gmail.com>
2024-07-15 02:28:14 +0900

    [Feat] #224 plan service에 나의 다가오는 일정 목록, 다녀온 일정 목록 api 추가

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/network/service/PlanService.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/network/service/PlanService.kt
@@ -4,6 +4,8 @@ import kr.tekit.lion.daongil.data.dto.remote.response.plan.openSchedule.OpenPlan
 import kr.tekit.lion.daongil.data.dto.remote.response.plan.PlaceSearchResultsResponse
 import kr.tekit.lion.daongil.data.dto.remote.response.plan.briefScheduleInfo.BriefScheduleInfoResponse
 import kr.tekit.lion.daongil.data.dto.remote.response.plan.myMainSchedule.MyMainScheduleResponse
+import kr.tekit.lion.daongil.data.dto.remote.response.plan.myScheduleElapsed.MyElapsedResponse
+import kr.tekit.lion.daongil.data.dto.remote.response.plan.myScheduleUpcoming.MyUpcomingsResponse
 import kr.tekit.lion.daongil.data.network.AuthType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -67,4 +69,17 @@ interface PlanService {
         @Part("planReviewReq") scheduleReview: RequestBody,
     )
 
+    // 다가오는 일정 목록
+    @GET("plan/my/not-complete")
+    suspend fun getMyUpcomingScheduleList(
+        @Query("size") size: Int,
+        @Query("page") page: Int
+    ): MyUpcomingsResponse
+
+    // 다녀온 일정 목록
+    @GET("plan/my/complete")
+    suspend fun getMyElapsedScheduleList(
+        @Query("size") size: Int,
+        @Query("page") page: Int
+    ): MyElapsedResponse
 }

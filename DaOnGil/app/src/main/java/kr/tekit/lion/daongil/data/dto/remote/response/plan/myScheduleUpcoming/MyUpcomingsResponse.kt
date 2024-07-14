af24e579a93561f3cf1fd10a3806ae24c39737a6
Boram <dev.lboram@gmail.com>
2024-07-15 02:26:25 +0900

    [Feat] #224 다가오는 일정 dto 생성

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/response/plan/myScheduleUpcoming/MyUpcomingsResponse.kt
@@ -0,0 +1,29 @@
+package kr.tekit.lion.daongil.data.dto.remote.response.plan.myScheduleUpcoming
+
+import kr.tekit.lion.daongil.domain.model.MyUpcomingScheduleInfo
+import kr.tekit.lion.daongil.domain.model.MyUpcomingSchedules
+
+data class MyUpcomingsResponse (
+    val code: Int,
+    val message: String,
+    val data: Data,
+){
+    fun toDomainModel(): MyUpcomingSchedules {
+        return MyUpcomingSchedules(
+            myUpcomingScheduleList = this.data.planResList.map {
+                MyUpcomingScheduleInfo(
+                    planId = it.planId,
+                    title = it.title,
+                    startDate = it.startDate,
+                    endDate = it.endDate,
+                    imageUrl = it.imageUrl ?: "",
+                    remainDate = it.remainDate
+                )
+            },
+            pageNo = data.pageNo,
+            pageSize = data.pageSize,
+            totalPages = data.totalPages,
+            last = data.last
+        )
+    }
+}

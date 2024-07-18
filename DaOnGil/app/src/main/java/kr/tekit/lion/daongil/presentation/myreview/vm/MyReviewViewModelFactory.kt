bd0d5bf51a05947d35d6022b6d8b18b3fb8c240e
rmadmsjm <dmswjdals12@naver.com>
2024-07-18 23:14:24 +0900

    [Feat] #225 MyReviewViewModelFactory에 UpdateMyPlaceReviewUseCase 추가

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myreview/vm/MyReviewViewModelFactory.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/myreview/vm/MyReviewViewModelFactory.kt
@@ -5,18 +5,21 @@ import androidx.lifecycle.ViewModelProvider
 import kr.tekit.lion.daongil.domain.repository.PlaceRepository
 import kr.tekit.lion.daongil.domain.usecase.place.DeleteMyPlaceReviewUseCase
 import kr.tekit.lion.daongil.domain.usecase.place.GetMyPlaceReviewUseCase
+import kr.tekit.lion.daongil.domain.usecase.place.UpdateMyPlaceReviewUseCase
 
 class MyReviewViewModelFactory : ViewModelProvider.Factory {
 
     private val placeRepository = PlaceRepository.create()
     private val getMyPlaceReviewUseCase = GetMyPlaceReviewUseCase(placeRepository)
     private val deleteMyPlaceReviewUseCase = DeleteMyPlaceReviewUseCase(placeRepository)
+    private val updateMyPlaceReviewUseCase = UpdateMyPlaceReviewUseCase(placeRepository)
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MyReviewViewModel::class.java)) {
             return MyReviewViewModel(
                 getMyPlaceReviewUseCase,
-                deleteMyPlaceReviewUseCase
+                deleteMyPlaceReviewUseCase,
+                updateMyPlaceReviewUseCase
             ) as T
         }
         throw IllegalArgumentException("unknown ViewModel class")

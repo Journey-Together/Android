45fc6367a5489a1f4c0d91d9b59b3c4babcfe0d4
rmadmsjm <dmswjdals12@naver.com>
2024-07-18 23:08:51 +0900

    [Feat] #225 MyPlaceReviewRequest 클래스 추가

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/data/dto/remote/request/MyPlaceReviewRequest.kt
@@ -0,0 +1,33 @@
+package kr.tekit.lion.daongil.data.dto.remote.request
+
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import kr.tekit.lion.daongil.data.network.LocalDateAdapter
+import kr.tekit.lion.daongil.domain.model.UpdateMyPlaceReview
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.time.LocalDate
+
+@JsonClass(generateAdapter = true)
+class MyPlaceReviewRequest(
+    val grade: Float,
+    val date: LocalDate,
+    val content: String,
+    val deleteImages: List<String>
+)
+
+fun UpdateMyPlaceReview.toRequestBody(): RequestBody {
+    val moshi = Moshi.Builder()
+        .add(LocalDateAdapter())
+        .build()
+
+    return moshi.adapter(MyPlaceReviewRequest::class.java).toJson(
+        MyPlaceReviewRequest(
+            this.grade,
+            this.date,
+            this.content,
+            this.deleteImages
+        )
+    ).toRequestBody("application/json".toMediaTypeOrNull())
+}

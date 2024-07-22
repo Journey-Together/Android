e4d1f8fc22b1d9ebca48ff2a0f80968018867814
Boram <dev.lboram@gmail.com>
2024-07-22 16:42:30 +0900

    [Feat] #234 입력된 여행정보 확인 기능 구현

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/ScheduleConfirmFormFragment.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/ScheduleConfirmFormFragment.kt
@@ -1,11 +1,17 @@
 package kr.tekit.lion.daongil.presentation.scheduleform.fragment
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import kr.tekit.lion.daongil.R
 import kr.tekit.lion.daongil.databinding.FragmentScheduleConfirmFormBinding
+import kr.tekit.lion.daongil.domain.model.DailySchedule
+import kr.tekit.lion.daongil.presentation.scheduleform.adapter.FormConfirmScheduleAdapter
 import kr.tekit.lion.daongil.presentation.scheduleform.vm.ScheduleFormViewModel
 import kr.tekit.lion.daongil.presentation.scheduleform.vm.ScheduleFormViewModelFactory
 
@@ -18,5 +24,56 @@ class ScheduleConfirmFormFragment : Fragment(R.layout.fragment_schedule_confirm_
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentScheduleConfirmFormBinding.bind(view)
+
+        initToolbar(binding)
+        initConfirmView(binding)
+        initButtonSubmitSchedule(binding)
+    }
+
+    private fun initToolbar(binding: FragmentScheduleConfirmFormBinding) {
+        binding.toolbarSConfirmForm.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun initConfirmView(binding: FragmentScheduleConfirmFormBinding){
+        val title = scheduleFormViewModel.getScheduleTitle()
+        val period = scheduleFormViewModel.getSchedulePeriod()
+        binding.apply {
+            textViewSConfirmFormTitle.text = getString(R.string.text_selected_title, title)
+            textViewSConfirmFormDate.text = getString(R.string.text_selected_period, period)
+        }
+
+        scheduleFormViewModel.schedule.observe(viewLifecycleOwner) {
+            if(it != null){
+                settingConfirmScheduleAdapter(binding, it)
+            }
+        }
+    }
+
+    private fun settingConfirmScheduleAdapter(
+        binding: FragmentScheduleConfirmFormBinding,
+        dailyScheduleList: List<DailySchedule>
+    ) {
+        binding.recyclerViewSConfirmForm.adapter = FormConfirmScheduleAdapter(dailyScheduleList)
+    }
+
+    private fun initButtonSubmitSchedule(binding: FragmentScheduleConfirmFormBinding){
+        binding.buttonSConfirmFormSubmit.setOnClickListener { view ->
+            scheduleFormViewModel.submitNewPlan{ _, requestFlag ->
+                if(requestFlag){
+                    requireActivity().setResult(Activity.RESULT_OK)
+                    requireActivity().finish()
+                }else{
+                    showSnackBar(view, "다시 시도해주세요")
+                }
+            }
+        }
+    }
+
+    private fun showSnackBar(view: View, message : String ){
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+            .setBackgroundTint(ContextCompat.getColor(requireActivity(), R.color.text_secondary))
+            .show()
     }
 }

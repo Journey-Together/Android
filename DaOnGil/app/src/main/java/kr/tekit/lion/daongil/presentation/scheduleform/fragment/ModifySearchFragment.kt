10be2dc9ad2798dc201b4f3a3e03941e43651e78
Boram <dev.lboram@gmail.com>
2024-07-29 16:48:58 +0900

    [Feat] #248 여행지 검색화면 추가

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/ModifySearchFragment.kt
@@ -0,0 +1,8 @@
+package kr.tekit.lion.daongil.presentation.scheduleform.fragment
+
+import androidx.fragment.app.Fragment
+import kr.tekit.lion.daongil.R
+
+
+class ModifySearchFragment : Fragment(R.layout.fragment_modify_search) {
+}

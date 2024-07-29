125ec661ab1513505bc384002836794d9f01d0ac
Boram <dev.lboram@gmail.com>
2024-07-29 14:22:36 +0900

    [Feat] #248 일정 수정 Navigation 추가

---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/ModifyScheduleConfirmFragment.kt
@@ -0,0 +1,6 @@
+package kr.tekit.lion.daongil.presentation.scheduleform.fragment
+
+import androidx.fragment.app.Fragment
+class ModifyScheduleConfirmFragment : Fragment() {
+
+}
---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/scheduleform/fragment/ModifyScheduleDetailsFragment.kt
@@ -0,0 +1,8 @@
+package kr.tekit.lion.daongil.presentation.scheduleform.fragment
+
+import androidx.fragment.app.Fragment
+
+
+class ModifyScheduleDetailsFragment : Fragment() {
+
+}

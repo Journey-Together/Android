1e46efa348401f863a536d768ea79850be9e235e
Boram <dev.lboram@gmail.com>
2024-08-03 15:22:50 +0900

    [Feat] #245 작성된 여행 후기 정보 API 연결

--- DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedulereview/vm/ModifyScheduleReviewViewModel.kt
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedulereview/vm/ModifyScheduleReviewViewModel.kt
@@ -1,17 +1,30 @@
 package kr.tekit.lion.daongil.presentation.schedulereview.vm
 
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import kr.tekit.lion.daongil.domain.model.ReviewImage
+import kr.tekit.lion.daongil.domain.model.ScheduleReviewInfo
+import kr.tekit.lion.daongil.domain.usecase.base.onError
+import kr.tekit.lion.daongil.domain.usecase.base.onSuccess
+import kr.tekit.lion.daongil.domain.usecase.plan.GetScheduleReviewInfoUseCase
 
-class ModifyScheduleReviewViewModel : ViewModel() {
+class ModifyScheduleReviewViewModel(
+    private val  getScheduleReviewUseCase: GetScheduleReviewInfoUseCase,
+    ) : ViewModel() {
 
     private val _imageList = MutableLiveData<List<ReviewImage>>()
     val imageList: LiveData<List<ReviewImage>> get() = _imageList
 
     private val _numOfImages = MutableLiveData<Int>(0)
     val numOfImages: LiveData<Int> get() = _numOfImages
+
+    private val _originalReview = MutableLiveData<ScheduleReviewInfo>()
+    val originalReview: LiveData<ScheduleReviewInfo> get() = _originalReview
 
     fun addNewReviewImage(newImage: ReviewImage){
         val currentImageList = _imageList.value?.toMutableList() ?: mutableListOf<ReviewImage>()
@@ -36,5 +49,30 @@ class ModifyScheduleReviewViewModel : ViewModel() {
     fun isMoreImageAttachable(): Boolean{
         val currentValue = _numOfImages.value ?: 0
         return currentValue in 0..3
+    }
+
+    fun getScheduleReviewInfo(planId: Long) = viewModelScope.launch {
+        getScheduleReviewUseCase(planId)
+            .onSuccess {
+                _originalReview.value = it
+                initReviewImages()
+            }.onError {
+                Log.d("getScheduleReviewInfo", "${it.message}")
+            }
+    }
+
+    private fun initReviewImages(){
+        _originalReview.value?.imageList?.let {
+            val reviewImages = it.map { imageUrl ->
+                ReviewImage(
+                    imageUrl = imageUrl,
+                    imageUri = Uri.parse(imageUrl),
+                    imagePath = null
+                )
+            }
+
+            _imageList.value = reviewImages
+            updateNumOfImages()
+        }
     }
 }
---
+++ DaOnGil/app/src/main/java/kr/tekit/lion/daongil/presentation/schedulereview/vm/ModifyScheduleReviewViewModelFactory.kt
@@ -0,0 +1,21 @@
+package kr.tekit.lion.daongil.presentation.schedulereview.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.tekit.lion.daongil.domain.repository.PlanRepository
+import kr.tekit.lion.daongil.domain.usecase.plan.GetScheduleReviewInfoUseCase
+
+class ModifyScheduleReviewViewModelFactory : ViewModelProvider.Factory {
+    private val getScheduleReviewInfoUseCase = GetScheduleReviewInfoUseCase(
+        PlanRepository.create()
+    )
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(ModifyScheduleReviewViewModel::class.java)){
+            return ModifyScheduleReviewViewModel(
+                getScheduleReviewInfoUseCase,
+            ) as T
+        }
+        else throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
